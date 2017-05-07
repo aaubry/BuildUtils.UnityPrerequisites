0db1240a03a5bdf9f4666c3b0fc511cc4622104a
Antoine Aubry <antoine@aaubry.net>
2017-05-07 19:05:31 +0100

    Do not distribute the files as a zip

--- install.ps1
+++ install.ps1
@@ -1,14 +1,2 @@
-$targets = @{
-  'Unity Subset v3.5' = 'C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v3.5\Profile'
-  'UnityVS' = 'C:\Program Files (x86)\MSBuild\SyntaxTree'
-}
-
-$shell = new-object -com shell.application
-$zip = $shell.NameSpace("$PSScriptRoot\package.zip")
-foreach($item in $zip.items())
-{
-  $target = $targets[$item.Name]
-  Write-Output "Extracting $($item.Name) to $target"
-  $d = mkdir $target -Force
-  $shell.Namespace($target).copyhere($item)
-}
+Copy-Item '.\Unity Subset v3.5' 'C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v3.5\Profile' -Recurse
+Copy-Item '.\UnityVS' 'C:\Program Files (x86)\MSBuild\SyntaxTree' -Recurse
